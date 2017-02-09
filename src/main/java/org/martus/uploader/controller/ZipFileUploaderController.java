e6ccd8de47b24a0770bdb131ded899f93ea08f04
Nima A <nimaa@benetech.org>
2017-02-09 17:29:59 -0500

    Changed controller to rest, for debugging purposes

--- src/main/java/org/martus/uploader/controller/ZipFileUploaderController.java
+++ src/main/java/org/martus/uploader/controller/ZipFileUploaderController.java
@@ -24,12 +24,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+@RestController
 public class ZipFileUploaderController 
 {
 //	private final StorageService storageService;
