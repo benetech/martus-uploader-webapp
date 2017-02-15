b606b7710f3dc6bbd439d0b4aa14df0997915e8a
Nima A <nimaa@benetech.org>
2017-02-14 19:10:28 -0500

    incremented version code

--- src/main/java/org/martus/uploader/MartusUploaderWebappApplication.java
+++ src/main/java/org/martus/uploader/MartusUploaderWebappApplication.java
@@ -32,7 +32,7 @@ public class MartusUploaderWebappApplication extends SpringBootServletInitialize
 	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-    	System.out.println("Running Martus uploader application via war 10");
+    	System.out.println("Running Martus uploader application via war 11");
 //    	init();
         return applicationBuilder.sources(MartusUploaderWebappApplication.class);
     }
