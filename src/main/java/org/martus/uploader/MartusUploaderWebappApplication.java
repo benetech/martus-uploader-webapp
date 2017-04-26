6fdab6892720bd12291916d97886e11be3f0fb6e
Nima A <nimaa@benetech.org>
2017-04-26 14:09:03 -0400

    Refactor cleanup

--- src/main/java/org/martus/uploader/MartusUploaderWebappApplication.java
+++ src/main/java/org/martus/uploader/MartusUploaderWebappApplication.java
@@ -2,25 +2,16 @@ package org.martus.uploader;
 
 import java.io.File;
 
-import org.martus.client.swingui.Martus;
 import org.martus.clientside.ClientPortOverride;
 import org.martus.uploader.storage.FileSystemStorageService;
-//import org.martus.common.crypto.MartusSecurity;
-//import org.martus.uploader.storage.FileSystemStorageService;
 import org.martus.uploader.storage.StorageProperties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
